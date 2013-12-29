5a8eab18e6a83d78ac06ced4fc06fecaa7bafe53
Imogen <imogen.wentworth@gmail.com>
2013-12-29 13:20:28 +0000

    Replace scroll function and get nav working

---
+++ config.rb
@@ -0,0 +1,6 @@
+http_path = "/"
+css_dir = "css"
+sass_dir = "css/sass"
+images_dir = "img"
+javascripts_dir = "js"
+require "compass-normalize"
---
+++ js/main.js
@@ -0,0 +1,80 @@
+// Initial load of page
+$(document).ready(function() {
+
+    // Make sure nav is always vertically centered
+    function repositionNav() {
+        var $windowHeight = $(window).height();
+        var $halfNavHeight = $('#nav').height() / 2;
+        var $windowCenter = ($windowHeight / 2);
+        var newTop = $windowCenter - $halfNavHeight;
+        $('#nav').css({"top": newTop});
+    }
+
+    // Dynamically assign welcome height
+    function sizeContent() {
+        var $welcomeHeight = $('#welcome').height() + 'px';
+        $('#work').add('#bg').css('top', $welcomeHeight);
+    }
+
+    // Scroll to all internal links
+    function smoothScroll() {
+        $('a[href^="#"]').on('click',function (event) {
+            event.preventDefault();
+
+            var target = this.hash,
+            $target = $(target);
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 600, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+    }
+
+    // Assign nav classes based on section visibility
+    function checkVisible(section, navLink) {
+        $(section).bind('inview', function (event, visible) {
+            if (visible === true) {
+                if ($(navLink).hasClass('fa-circle-o')) {
+                    $(navLink).addClass('fa-circle').removeClass('fa-circle-o');
+                }
+            } else {
+                if ($(navLink).hasClass('fa-circle')) {
+                    $(navLink).addClass('fa-circle-o').removeClass('fa-circle');
+                }
+            }
+        });
+    }
+
+    // Masonry options
+    $(function(){
+        var $container = $('.grid');
+        $container.imagesLoaded(function() {
+            $container.masonry({
+                columnWidth: 320,
+                itemSelector: '.item',
+                gutter: 10
+            });
+        });
+    });
+
+    repositionNav();
+    sizeContent();
+    smoothScroll();
+
+    // Recalculate nav and welcome every resize of window
+    $(window).resize(function() {
+        repositionNav();
+        sizeContent();
+    });
+
+    // Check if a section is in viewport on scroll
+    $(window).scroll(function() {
+        checkVisible('#welcome', '#wel-btn');
+        checkVisible('#developer', '#dev-btn');
+        checkVisible('#creative', '#cre-btn');
+        checkVisible('#technologist', '#tech-btn');
+    });
+
+});
