faa71fceab0811e9e2e312d6554ef54ad66c1c22
Imogen <imogen.wentworth@gmail.com>
2014-03-13 15:56:30 -0700

    Apply lazyload to all .lazy elements

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ $(document).ready(function() {
 
     // Lazyload images
     $(function() {
-        $('img.lazy').add('#welcome').lazyload({
+        $('.lazy').lazyload({
             effect : 'fadeIn'
         });
     });
