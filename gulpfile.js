8b11c1f1e76a3d0bf93dff9008846c3a6bad9393
Imogen <imogen.wentworth@gmail.com>
2015-07-16 13:41:14 -0700

    Add React project/remove animations

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
@@ -7,27 +6,14 @@ var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var connect = require('gulp-connect');
 
-gulp.task('lint', function() {
-  return gulp.src('js/main.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
-
 gulp.task('sass', function() {
   return gulp.src('sass/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('js/main.js')
-    .pipe(rename('main.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('js'));
-});
-
 gulp.task('styles', function() {
-  return gulp.src(['css/libs/*.css', 'css/main.css'])
+  return gulp.src(['css/libs/*.css', 'bower_components/minimal-devices/css/*.css', 'css/main.css'])
     .pipe(concat('all.css'))
     .pipe(gulp.dest('css'))
     .pipe(rename('all.min.css'))
@@ -36,7 +22,6 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('js/**/*.js', ['lint', 'scripts']);
   gulp.watch('sass/**/*.scss', ['sass']);
   gulp.watch('css/main.css', ['styles']);
 });
@@ -45,4 +30,4 @@ gulp.task('connect', function() {
   connect.server({ livereload: true });
 });
 
-gulp.task('default', ['sass', 'scripts', 'styles', 'watch', 'connect']);
+gulp.task('default', ['sass', 'styles', 'watch', 'connect']);
